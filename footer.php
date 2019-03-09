2211a03b4917ff6a9a7b7f4b4627af7e2eb9af3a
halcyonseeker <vesperous@protonmail.com>
2019-03-09 10:32:15 +0700

    Simplified structure, added content

---
+++ footer.php
@@ -0,0 +1,5 @@
+<?php
+echo "<hr>";
+echo "<h4><a href = "https://halcyonseeker.github.io" >home</a></h4>";
+echo "<p>vesperous at protonmail dot com</p>";
+?>
---
+++ sidebar.php
@@ -0,0 +1,13 @@
+<?php
+echo "<ul>";
+echo "<h3>Pages</h3>";
+echo "<li><a href = "https://halcyonseeker.github.io/weblog/index.html">Web Log</a></li>";
+echo "<li><a href = "https://halcyonseeker.github.io/projects/index.html">Projects</a></li>";
+echo " <li><a href = "https://halcyonseeker.github.io/reading.html">Good Things to Read</a></li>";
+echo " <!--<h3>Links</h3>-->";
+echo " <!--<li><a target = "_blank" href = "https://www.github.com/halcyonseeker">GitHub</a></li>-->";
+echo " <!--<li><a target = "_blank" href = "https://www.minds.com/ZenithBound">Minds</a></li>-->";
+echo " <!--<li><a target = "_blank" href = "https://hackaday.io/halcyonseeker">Hackaday</a></li>-->";
+echo " <!--<li><a target = "_blank" href = "https://www.youtube.com">YouTube</a></li>-->";
+echo "</ul>";
+?>
